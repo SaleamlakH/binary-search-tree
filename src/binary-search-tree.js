96cb7eb0b02b502cff99b58de5adce3017d6fb5d
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-13 18:05:50 +0300

    feat: implement BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,3 +1,33 @@
-export const testFun = () => {
-  return 'Its ready';
-};
+import { Node } from './node.js';
+
+export class BinarySearchTree {
+  #root;
+
+  constructor(array) {
+    array = this.#uniqueAndSort(array);
+    this.#root = this.#buildTree(array);
+  }
+
+  get root() {
+    return this.#root;
+  }
+
+  #uniqueAndSort(array) {
+    return array
+      .filter((value, index) => array.indexOf(value) === index)
+      .sort((a, b) => a - b);
+  }
+
+  #buildTree(array) {
+    if (!array.length) return null;
+
+    let mid = Math.floor((array.length - 1) / 2);
+
+    // build node
+    let root = new Node(array[mid]);
+    root.left = this.#buildTree(array.slice(0, mid));
+    root.right = this.#buildTree(array.slice(mid + 1));
+
+    return root;
+  }
+}
--- tests/binary-search-tree.test.js
+++ tests/binary-search-tree.test.js
@@ -1,3 +1,22 @@
-import { testFun } from 'binary-search-tree';
+import { BinarySearchTree } from 'binary-search-tree';
 
-console.log(testFun());
+describe('Binary search tree', () => {
+  let array;
+
+  beforeEach(() => {
+    array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+    // unique and sorted version
+    // [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
+  });
+
+  describe('Build tree', () => {
+    test('Returns the root node', () => {
+      let root = new BinarySearchTree(array).root;
+
+      expect(root.value).toBe(8);
+      expect(root.left.value).toBe(4);
+      expect(root.right.value).toBe(67);
+    });
+  });
+});
